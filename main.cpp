cc7919499d23de60ce06f07510937b91e97ca740
acharyabibash <acharabibash@outlook.com>
2022-09-15 13:41:31 +0545

    Quizma Heroes Successfully Run
    Added openal32.dll plus other Source and Headers Files And Added Files in Resources Folders and Added All .dll extension files

--- main.cpp
+++ main.cpp
@@ -1,24 +1,9 @@
-#include <SFML/Graphics.hpp>
+#include "Games.hpp"
+#include "DEFINITIONS.hpp"
 
 int main()
 {
-    sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-    sf::CircleShape shape(100.f);
-    shape.setFillColor(sf::Color::Green);
+	Quizma::Games(SCREEN_WIDTH, SCREEN_HEIGHT, "QUIZMA HEROES");
 
-    while (window.isOpen())
-    {
-        sf::Event event;
-        while (window.pollEvent(event))
-        {
-            if (event.type == sf::Event::Closed)
-                window.close();
-        }
-
-        window.clear();
-        window.draw(shape);
-        window.display();
-    }
-
-    return 0;
+	return EXIT_SUCCESS;
 }
